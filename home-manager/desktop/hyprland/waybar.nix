d0878e3b8d6bc647b0c743103e38fc8a51a8236f
Dawid Krzoska <dawidwilk19999@gmail.com>
2024-04-21 21:30:28 +0200

    add waybar.nix

---
+++ home-manager/desktop/hyprland/waybar.nix
@@ -0,0 +1,175 @@
+{
+  outputs,
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}: let
+  # Dependencies
+  pavucontrol = "${pkgs.pavucontrol}/bin/pavucontrol";
+  hyprland = config.wayland.windowManager.hyprland.package;
+in {
+  # Let it try to start a few more times
+  systemd.user.services.waybar = {
+    Unit.StartLimitBurst = 30;
+  };
+  programs.waybar = {
+    enable = true;
+    });
+    systemd.enable = true;
+    settings = {
+      primary = {
+        exclusive = false;
+        passthrough = false;
+        height = 40;
+        margin = "6";
+        position = "top";
+        modules-left = [
+            "hyprland/workspaces"
+            "hyprland/submap"
+          ];
+
+        modules-center = [
+          "cpu"
+          "memory"
+          "pulseaudio"
+        ];
+
+        modules-right = [
+          "network"
+          "tray"
+          "clock"
+        ];
+
+        clock = {
+          interval = 1;
+          format = "{:%d/%m %H:%M:%S}";
+          format-alt = "{:%Y-%m-%d %H:%M:%S %z}";
+          on-click-left = "mode";
+          tooltip-format = ''
+            <big>{:%Y %B}</big>
+            <tt><small>{calendar}</small></tt>'';
+        };
+
+        cpu = {
+          format = "  {usage}%";
+        };
+
+        pulseaudio = {
+          format = "{icon}  {volume}%";
+          format-muted = "   0%";
+          format-icons = {
+            headphone = "󰋋";
+            headset = "󰋎";
+            portable = "";
+            default = [
+              ""
+              ""
+              ""
+            ];
+          };
+          on-click = pavucontrol;
+        };
+        idle_inhibitor = {
+          format = "{icon}";
+          format-icons = {
+            activated = "󰒳";
+            deactivated = "󰒲";
+          };
+        };
+        battery = {
+          bat = "BAT0";
+          interval = 10;
+          format-icons = [
+            "󰁺"
+            "󰁻"
+            "󰁼"
+            "󰁽"
+            "󰁾"
+            "󰁿"
+            "󰂀"
+            "󰂁"
+            "󰂂"
+            "󰁹"
+          ];
+          format = "{icon} {capacity}%";
+          format-charging = "󰂄 {capacity}%";
+          onclick = "";
+        };
+        "sway/window" = {
+          max-length = 20;
+        };
+        network = {
+          interval = 3;
+          format-wifi = "   {essid}";
+          format-ethernet = "󰈁 Connected";
+          format-disconnected = "";
+          tooltip-format = ''
+            {ifname}
+            {ipaddr}/{cidr}
+            Up: {bandwidthUpBits}
+            Down: {bandwidthDownBits}'';
+          on-click = "";
+        };
+      };
+    };
+    # Cheatsheet:
+    # x -> all sides
+    # x y -> vertical, horizontal
+    # x y z -> top, horizontal, bottom
+    # w x y z -> top, right, bottom, left
+    style =''
+        * {
+          font-family: FiraCode
+          font-size: 12pt;
+          padding: 0;
+          margin: 0 0.4em;
+        }
+
+        window#waybar {
+          padding: 0;
+          border-radius: 0.5em;
+          /*
+          background-color: ${toRGBA colors.surface "0.7"};
+          color: ${colors.on_surface};
+          */
+        }
+        .modules-left {
+          margin-left: -0.65em;
+        }
+        .modules-right {
+          margin-right: -0.65em;
+        }
+
+        #workspaces button {
+        /*
+          background-color: ${colors.surface};
+          color: ${colors.on_surface};
+          */
+          padding-left: 0.4em;
+          padding-right: 0.4em;
+          margin-top: 0.15em;
+          margin-bottom: 0.15em;
+        }
+        /*
+        #workspaces button.hidden {
+          background-color: ${colors.surface};
+          color: ${colors.on_surface_variant};
+        }
+        
+        #workspaces button.focused,
+        #workspaces button.active {
+          background-color: ${colors.tertiary};
+          color: ${colors.on_tertiary};
+        }
+        */
+
+        #clock {
+          padding-right: 1em;
+          padding-left: 1em;
+          border-radius: 0.5em;
+        }
+      '';
+  };
+}
